a14bd04015acff30980cf00e09fefaf2b23426ac
matahombres <motoreta98@gmail.com>
2023-07-08 22:25:55 +0200

    Add fast deposit to ATM

--- web/src/views/ATM/ATM.tsx
+++ web/src/views/ATM/ATM.tsx
@@ -5,14 +5,14 @@ import styled from '@emotion/styled';
 import { useConfig } from '@hooks/useConfig';
 import { Paper, Stack } from '@mui/material';
 import { ATMInput } from '@typings/Account';
-import { AccountEvents } from '@typings/Events';
+import { AccountEvents, CashEvents } from '@typings/Events';
 import { defaultWithdrawOptions } from '@utils/constants';
 import { formatMoney } from '@utils/currency';
 import { fetchNui } from '@utils/fetchNui';
 import theme from '@utils/theme';
 import { AnimatePresence } from 'framer-motion';
 import { useAtom } from 'jotai';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { motion } from 'framer-motion';
 import { useNuiEvent } from 'react-fivem-hooks';
@@ -35,13 +35,25 @@ const AccountBalance = styled(Heading6)`
   font-weight: ${theme.typography.fontWeightLight};
 `;
 
+const HeaderContainer = styled(Stack)`
+  display: flex;
+  flex-direction: row;
+`;
+
 const Header = styled(Stack)`
   margin-bottom: ${theme.spacing(7)};
+  flex: 1;
 `;
 
 const WithdrawText = styled(Heading6)`
   display: block;
   padding-bottom: ${theme.spacing(1.5)};
+`;
+
+const DepositText = styled(Heading6)`
+  display: block;
+  padding-bottom: ${theme.spacing(1.5)};
+  margin-top: 40px;
 `;
 
 const WithdrawContainer = styled.div`
@@ -54,12 +66,18 @@ const WithdrawContainer = styled.div`
 const ATM = () => {
   const { t } = useTranslation();
   const config = useConfig();
+  const [currentCash, setCurrentCash] = useState(0);
+  const [success, setSuccess] = useState('');
   const withdrawOptions = config?.atms?.withdrawOptions ?? defaultWithdrawOptions;
   const [isLoading, setIsLoading] = useState(false);
   const [accountBalance] = useAtom(defaultAccountBalance);
   const [, updateAccounts] = useAtom(accountsAtom);
 
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    fetchNui<number>(CashEvents.GetMyCash).then((cash) => setCurrentCash(cash ?? 0));
+  }, [success]);
 
   useNuiEvent<boolean>({
     event: 'setVisibleATM',
@@ -70,13 +88,34 @@ const ATM = () => {
   const handleWithdraw = (amount: number) => {
     const payload: ATMInput = {
       amount,
-      message: 'Withdrew ' + amount + ' from an ATM.',
+      message: t('Withdrew {{amount}} from account.', {
+        amount: formatMoney(amount, config.general),
+      }),
     };
 
     setIsLoading(true);
     // TODO: Update this with cards implementation
     fetchNui(AccountEvents.WithdrawMoney, payload)
-      .then(() => updateAccounts())
+      .then(() => {
+        setCurrentCash(currentCash + amount);
+        updateAccounts();
+      })
+      .finally(() => setIsLoading(false));
+  };
+  const handleDeposit = (amount: number) => {
+    const payload: ATMInput = {
+      amount,
+      message: t('Deposited {{amount}} into account.', {
+        amount: formatMoney(amount, config.general),
+      }),
+    };
+
+    setIsLoading(true);
+    fetchNui<ATMInput>(AccountEvents.DepositMoney, payload)
+      .then(() => {
+        setCurrentCash(currentCash - amount);
+        updateAccounts();
+      })
       .finally(() => setIsLoading(false));
   };
 
@@ -86,10 +125,16 @@ const ATM = () => {
         <AnimationContainer>
           <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} exit={{ scale: 0 }}>
             <Container elevation={4}>
-              <Header>
-                <AccountBalance>{t('Account balance')}</AccountBalance>
-                <Heading2>{formatMoney(accountBalance ?? 0, config.general)}</Heading2>
-              </Header>
+              <HeaderContainer>
+                <Header>
+                  <AccountBalance>{t('Account balance')}</AccountBalance>
+                  <Heading2>{formatMoney(accountBalance ?? 0, config.general)}</Heading2>
+                </Header>
+                <Header>
+                  <AccountBalance>{t('Current cash')}</AccountBalance>
+                  <Heading2>{formatMoney(currentCash ?? 0, config.general)}</Heading2>
+                </Header>
+              </HeaderContainer>
 
               <WithdrawText>{t('Quick withdraw')}</WithdrawText>
               <WithdrawContainer>
@@ -99,6 +144,19 @@ const ATM = () => {
                     onClick={() => handleWithdraw(value)}
                     data-value={value}
                     disabled={value > (accountBalance ?? 0) || isLoading}
+                  >
+                    {formatMoney(value, config.general)}
+                  </Button>
+                ))}
+              </WithdrawContainer>
+              <DepositText>{t('Quick deposit')}</DepositText>
+              <WithdrawContainer>
+                {withdrawOptions.map((value) => (
+                  <Button
+                    key={value}
+                    onClick={() => handleDeposit(value)}
+                    data-value={value}
+                    disabled={value > (currentCash ?? 0) || isLoading}
                   >
                     {formatMoney(value, config.general)}
                   </Button>
